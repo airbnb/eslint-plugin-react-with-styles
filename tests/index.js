8ce877df568dacb9ecee6987c6151858902dd255
Jordan Harband <ljharb@gmail.com>
2019-08-13 14:25:10 -0700

    [Deps] add `eslint` `v6` support

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const plugin = require('..');
-
 const expect = require('chai').expect;
 const fs = require('fs');
 const path = require('path');
 
+const plugin = require('..');
+
 const ruleFiles = fs.readdirSync(path.resolve(__dirname, '../lib/rules/'))
-  .map(f => path.basename(f, '.js'));
+  .map((f) => path.basename(f, '.js'));
 
 describe('all rule files are exported by the plugin', () => {
   ruleFiles.forEach((ruleName) => {
